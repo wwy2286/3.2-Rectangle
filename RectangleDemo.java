e6a898908eea5f6809790f9ddbb7065cb4f8e70e
William <wwy2286@gmail.com>
2017-03-05 23:18:15 -0500

    3rd commit with comments

--- RectangleDemo.java
+++ RectangleDemo.java
@@ -13,25 +13,37 @@ William Yu <br>
 wwy2286@gmail.com <br>
 purpose of program: To use JavaFX to find the area and perimeter of the rectangle
 */
+//creates class rectangledemo and let it have attributes of application
 public class RectangleDemo extends Application {
+	// set the canvass for javafx
 	public void start(Stage primaryStage){
+		// create new dialog box
 		TextInputDialog input = new TextInputDialog();
+		// set title
 		input.setTitle("Rectangle Application");
+		// set header
 		input.setHeaderText("Input Width");
+		// makes sure the dialog box is showing
 		input.showAndWait();
+		// ask for input for width
 		double width = Double.parseDouble(input.getResult());
-
+		// create new dialog box for height and set title
 		input.setTitle("Rectangle Application");
+		// set header
 		input.setHeaderText("Input Height");
+		// makes sure the dialog box is showing
 		input.showAndWait();
+		// set double height to user input
 		double height = Double.parseDouble(input.getResult());
-
+		// create new alert box to show on screen
 		Alert output = new Alert(AlertType.INFORMATION);
+		// set title for the alert box
 		output.setTitle("Rectangle Application");
+		// set header and calculates the perimiter
 		output.setHeaderText("The perimeter of the rectangle is " + (2*height+2*width));
 		output.showAndWait();
-		
 		output.setTitle("Rectangle Application");
+		//set header and calculates the area
 		output.setHeaderText("The area of the rectangle is " + width*height);
 		output.showAndWait();
 }
